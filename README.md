8bbd4a37ba4780ad19b83b0504d3423dff2101dc
Alexandra <rudakalexx@gmail.com>
2020-07-21 09:21:48 +0300

    Change tests

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 **Patterns iBank** [![Build status](https://ci.appveyor.com/api/projects/status/ycyeej9rwanpqqxq?svg=true)](https://ci.appveyor.com/project/Flayka/patternsibank)
 
-* Время, затраченное на ручное тестирование (минут): x
-* Время, затраченное на автоматизацию (минут): y
+* Время, затраченное на ручное тестирование (минут): 90
+* Время, затраченное на автоматизацию (минут): 180
--- src/test/java/ru/netology/web/AuthTest.java
+++ src/test/java/ru/netology/web/AuthTest.java
@@ -7,7 +7,6 @@ import ru.netology.data.DataGenerator;
 import ru.netology.data.RegistrationDto;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -24,8 +23,8 @@ public class AuthTest {
         $("[name='login']").setValue(validActiveUser.getLogin());
         $("[name='password']").setValue(validActiveUser.getPassword());
         $("[data-test-id='action-login']").click();
-        $("[class='heading heading_size_l heading_theme_alfa-on-white']").waitUntil(exist, 5000);
-        $("[class='heading heading_size_l heading_theme_alfa-on-white']").shouldHave(matchesText("Личный кабинет"));
+        $(".heading").waitUntil(exist, 5000);
+        $(".heading").shouldHave(matchesText("Личный кабинет"));
     }
 
     @Test
@@ -34,7 +33,7 @@ public class AuthTest {
         $("[name='login']").setValue(validBlockedUser.getLogin());
         $("[name='password']").setValue(validBlockedUser.getPassword());
         $("[data-test-id='action-login']").click();
-        $(byText("Ошибка")).waitUntil(Condition.visible, 15000);
+        $("[data-test-id='error-notification']").waitUntil(Condition.visible, 15000);
     }
 
     @Test
@@ -43,8 +42,7 @@ public class AuthTest {
         $("[name='login']").setValue(inCorrectLogin.getLogin());
         $("[name='password']").setValue(inCorrectLogin.getPassword());
         $("[data-test-id='action-login']").click();
-
-        $(byText("Ошибка")).waitUntil(Condition.visible, 15000);
+        $("[data-test-id='error-notification']").waitUntil(Condition.visible, 15000);
     }
 
     @Test
@@ -53,8 +51,7 @@ public class AuthTest {
         $("[name='login']").setValue(inCorrectPassword.getLogin());
         $("[name='password']").setValue(inCorrectPassword.getPassword());
         $("[data-test-id='action-login']").click();
-
         $(".notification_status_error").waitUntil(visible, 5000);
-        $(".notification_visible[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Неверно указан логин или пароль"));
+        $("[data-test-id=error-notification]").shouldHave(Condition.matchesText("Ошибка! Неверно указан логин или пароль"));
     }
 }
